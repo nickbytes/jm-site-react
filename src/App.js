410e341878b1ce6422dd7668c741ae6366bcce76
Jason Mandel <jmandel1027@gmail.com>
2017-07-20 13:17:28 -0400

    progress

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Navigation from './Components/Navigation'
+
+
+const App = () => {
+
+  return (
+
+      <Navigation />
+
+  )
+}
+
+export default App
